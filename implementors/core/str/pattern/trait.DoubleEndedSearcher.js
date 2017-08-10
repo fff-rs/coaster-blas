2c7c3d26a419def20c3939b24ff70bc7c417a256
Sir Mergealot <sirmergealot@spearow.io>
2017-08-10 20:54:27 +0000

    doc/automatic: update

--- implementors/core/str/pattern/trait.DoubleEndedSearcher.js
+++ implementors/core/str/pattern/trait.DoubleEndedSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['enum_primitive'] = [];
+implementors["coaster_blas"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/rustc_serialize/hex/trait.FromHex.js
+++ implementors/rustc_serialize/hex/trait.FromHex.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc_serialize'] = [];
+implementors["coaster_blas"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
